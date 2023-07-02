3f7adb7b4b692c3c6487444904ed166c0244689c
Isaac-Franklyn <officialisaacfranklyn@gmail.com>
2023-07-02 12:04:17 +0530

    4-write-to-file.js has been added

---
+++ 02-async-js/easy/4-write-to-file.js
@@ -0,0 +1,17 @@
+/* ## Write to a file
+Using the fs library again, try to write to the contents of a file.
+You can use the fs library to as a black box, the goal is to understand async tasks.8 */
+
+const fs = require('fs');
+const content = "isaac is trying to code in js";
+const filePath = "easy/ fileToPrint.txt";
+
+fs.writeFile(filePath, content, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  
+    console.log('File has been written successfully.');
+  });
+
